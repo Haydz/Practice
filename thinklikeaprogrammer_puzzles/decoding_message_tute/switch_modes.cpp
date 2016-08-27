44cfab847e64bbba726d77ae5ecbd32c8ae7a366
Haydn <haydnit@gmail.com>
2016-08-27 11:22:05 -0400

    decoding added. This reads a digit by character, converts to digit and returns a integer. this works by multiplying 10 to the digit if more digits are added.

---
+++ thinklikeaprogrammer_puzzles/decoding_message_tute/switch_modes.cpp
@@ -0,0 +1,63 @@
+/*
+
+the point with this pieceo fthe puzzle,
+
+is that when a a vlue decodes to zero, the mode switches
+THe switching order is:
+
+Upper Case - > Lower Case
+Lower Case - > Punctuation
+Punctuation - > Upper Case
+
+
+*/
+
+
+#include <iostream>
+#include <stdlib.h>
+using std::cin;
+using std::cout;
+
+
+int main()
+{
+
+enum modeType {UPPERCASE, LOWERCASE, PUNCTUATION};
+int number;
+modeType mode = UPPERCASE;
+cout << "Enter some numbers ending with -1: > ";
+do {
+
+    cin >> number;
+    cout <<"Number read: " << number;
+    switch (mode) {
+        case UPPERCASE:
+            number = number % 27;
+            cout << ". Modulo 27: " << number << ". ";
+            if(number == 0){
+            cout << "Switch to LOWERCASE";
+            mode = LOWERCASE;
+            }
+            break;
+         case LOWERCASE:
+            number = number % 27;
+            cout <<". Modulo 27: " << number << ". ";
+            if (number == 0){
+                cout << "Switch to PUNCTUATION";
+                mode = PUNCTUATION;
+
+            }
+            break;
+           case PUNCTUATION:
+            number = number & 9;
+            cout << ". Modulo 9: " << number << ". ";
+            if(number == 0){
+                cout << "Switch to UPPERCASE";
+                mode = UPPERCASE;
+            }
+            break;
+       }
+       cout << "\n";
+}while (number != -1);
+}
+
